41a4baa209c644764e57972c367d891f714e6218
lampsgit <134364052+lampsgit@users.noreply.github.com>
2023-06-03 14:50:48 +0530

    added kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,16 +1,22 @@
 import matplotlib.pyplot as plt
-from sklearn.cluster import Kmeans
-X=[3,4,4,5,6,10,10,11,12,14]
-Y=[16,21,25,29,22,19,21,25,21,25]
-data=list(zip(X,Y))
-inertias=[]
-for i in range(1,11):
-    km=Kmeans(n_clusters=i)
+from sklearn.cluster import KMeans
+
+X = [3, 4, 4, 5, 6, 10, 10, 11, 12, 14]
+Y = [16, 21, 25, 29, 22, 19, 21, 25, 21, 25]
+data = list(zip(X, Y))
+
+inertias = []
+for i in range(1, 11):
+    km = KMeans(n_clusters=i)
     km.fit(data)
-    inertias.append(km.inertia)
-plt.plot(range(1,11),inertias,marker='*')
-plt.title("Elbow")
-plt.xlabel("no of clusters")
-plt.ylabel("Inertias")
+    inertias.append(km.inertia_)
+
+plt.plot(range(1, 11), inertias, marker='.')
+plt.title("Elbow Method")
+plt.xlabel("Number of Clusters")
+plt.ylabel("Inertia")
 plt.show()
-km=Kmeans(n_clusters=2)
+
+km = KMeans(n_clusters=2)
+km.fit(data)
+plt.show()
